4a312923caef13b9f01d8f6c4da7ba0cb6670eca
Marcos Paliari <marcos@paliari.com.br>
2018-11-19 12:35:12 -0200

    add textRotation

--- src/Fpdf.php
+++ src/Fpdf.php
@@ -1050,6 +1050,21 @@ class Fpdf
         return '';
     }
 
+    public function textRotation($x, $y, $txt, $txt_angle, $font_angle = 0)
+    {
+        $font_angle += 90 + $txt_angle;
+        $txt_angle  *= M_PI / 180;
+        $font_angle *= M_PI / 180;
+        $txt_dx     = cos($txt_angle);
+        $txt_dy     = sin($txt_angle);
+        $font_dx    = cos($font_angle);
+        $font_dy    = sin($font_angle);
+        $s          = sprintf('BT %.2F %.2F %.2F %.2F %.2F %.2F Tm (%s) Tj ET', $txt_dx, $txt_dy, $font_dx, $font_dy, $x * $this->k, ($this->h - $y) * $this->k, $this->_escape($txt));
+        if ($this->colorFlag)
+            $s = 'q ' . $this->textColor . ' ' . $s . ' Q';
+        $this->_out($s);
+    }
+
     protected function _dochecks()
     {
         //Check mbstring overloading
