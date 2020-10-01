50b4e2687cbe8e0e046c5c40695e0025b83022d4
Marcos Paliari <marcos@paliari.com.br>
2020-10-01 19:00:26 -0300

    use method mb_check_encoding for check utf8

--- src/Fpdf.php
+++ src/Fpdf.php
@@ -6,7 +6,6 @@ use Exception;
 
 class Fpdf
 {
-
     const VERSION = '1.81';
 
     protected $page;               // current page number
@@ -1944,16 +1943,7 @@ class Fpdf
 
     protected function isUtf8($string)
     {
-        return preg_match('%^(?:
-		  [\x09\x0A\x0D\x20-\x7E]			# ASCII
-		| [\xC2-\xDF][\x80-\xBF]			# non-overlong 2-byte
-		|  \xE0[\xA0-\xBF][\x80-\xBF]		# excluding overlongs
-		| [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2} # straight 3-byte
-		|  \xED[\x80-\x9F][\x80-\xBF]		# excluding surrogates
-		|  \xF0[\x90-\xBF][\x80-\xBF]{2}	# planes 1-3
-		| [\xF1-\xF3][\x80-\xBF]{3}		 	# planes 4-15
-		|  \xF4[\x80-\x8F][\x80-\xBF]{2}	# plane 16
-	)*$%xs', $string);
+        return mb_check_encoding($string, 'UTF-8');
     }
 
     protected $extgstates = [];
